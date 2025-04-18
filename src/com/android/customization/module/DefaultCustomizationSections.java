bdf4b1c01532fec957a5adfadf18ba61f9ecf19e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-18 20:12:57 +0800

    ThemePicker: Add lockscreen clock font customization
    Change-Id: I398038c80ed506a453de52f4ec6ed8ce31c1ba4f
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -2,6 +2,8 @@ package com.android.customization.module;
 
 import android.app.WallpaperManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
@@ -18,6 +20,8 @@ import com.android.customization.model.iconshape.IconShapeManager;
 import com.android.customization.model.iconshape.IconShapeSectionController;
 import com.android.customization.model.mode.DarkModeSectionController;
 import com.android.customization.model.mode.DarkModeSnapshotRestorer;
+import com.android.customization.model.lockfont.LockFontSectionController;
+import com.android.customization.model.lockfont.LockFontManager;
 import com.android.customization.model.theme.OverlayManagerCompat;
 import com.android.customization.model.themedicon.ThemedIconSectionController;
 import com.android.customization.model.themedicon.ThemedIconSwitchProvider;
@@ -57,6 +61,9 @@ import com.android.wallpaper.util.DisplayUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /** {@link CustomizationSections} for the customization picker. */
 public final class DefaultCustomizationSections implements CustomizationSections {
@@ -190,6 +197,29 @@ public final class DefaultCustomizationSections implements CustomizationSections
                                         mKeyguardQuickAffordancePickerViewModelFactory)
                                         .get(KeyguardQuickAffordancePickerViewModel.class),
                                 lifecycleOwner));
+
+                String clockFaceJson = Settings.Secure.getString(
+                        activity.getContentResolver(), "lock_screen_custom_clock_face");
+
+                boolean shouldAddLockFontSection = false;
+                if (clockFaceJson == null || clockFaceJson.isEmpty()) {
+                    shouldAddLockFontSection = true;
+                } else {
+                    try {
+                        JSONObject clockFace = new JSONObject(clockFaceJson);
+                        if (!clockFace.has("clockId") || "DEFAULT".equals(clockFace.optString("clockId"))) {
+                            shouldAddLockFontSection = true;
+                        }
+                    } catch (JSONException e) {
+                        Log.w("CustomizationSections", "Failed to parse lock_screen_custom_clock_face: " + clockFaceJson, e);
+                    }
+                }
+
+                if (shouldAddLockFontSection) {
+                    sectionControllers.add(new LockFontSectionController(
+                            LockFontManager.getInstance(activity, new OverlayManagerCompat(activity)),
+                            sectionNavigationController));
+                }
 
                 // Icon pack selection section.
                 sectionControllers.add(new IconPackSectionController(
